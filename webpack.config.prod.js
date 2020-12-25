53f75f2bed4567f82033e2f29c7361e5122e652d
Jonathan Rubins <jonrubins@gmail.com>
2020-12-25 16:29:37 -0600

    Fix production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,6 @@
-const DotenvPlugin = require('webpack-dotenv-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require('webpack')
 
 const buildConfig = require('./buildConfig')
@@ -8,8 +8,34 @@ const buildConfig = require('./buildConfig')
 module.exports = {
   bail: true,
   context: __dirname,
+  devtool: 'source-map',
   entry: {
-    main: buildConfig.paths.app.mainJs,
+    main: buildConfig.paths.entry,
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        include: [buildConfig.paths.src],
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        include: [buildConfig.paths.src],
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              // Applies postcss-loader to @imported resources.
+              importLoaders: 1,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
   },
   output: {
     chunkFilename: 'js/[name].[chunkhash].js',
@@ -17,90 +43,35 @@ module.exports = {
     path: buildConfig.paths.dist,
     publicPath: '/',
   },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        include: [
-          buildConfig.paths.app.base,
-        ],
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.scss$/,
-        include: [
-          buildConfig.paths.app.base,
-        ],
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader?importLoaders=1',
-            'postcss-loader',
-            'sass-loader',
-          ],
-        }),
-      },
-    ],
-  },
   plugins: [
-    new DotenvPlugin({
-      sample: '.env',
-      path: '.env',
-    }),
+    // This is a shorthand plugin for the DefinePlugin.
+    new webpack.EnvironmentPlugin([
+      'APP_BASE_URL',
+      'FACEBOOK_APP_ID',
+      'GA_PROPERTY',
+      'NODE_ENV',
+    ]),
     new HtmlWebpackPlugin({
-      favicon: buildConfig.paths.app.favicon,
-      template: buildConfig.paths.app.html,
+      favicon: buildConfig.paths.public.favicon,
+      // "inject: true" places all JavaScript resources at the bottom of the body element.
+      inject: true,
+      template: buildConfig.paths.public.html,
     }),
 
-    // Ignore locales from moment.
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-
-    // Need this to preserve the IDs of Webpack modules between builds. Otherwise having new imports in the main bundle
-    // will cache-bust the vendor bundle.
-    new webpack.HashedModuleIdsPlugin(),
-
-    // Enable scope-hoisting.
-    new webpack.optimize.ModuleConcatenationPlugin(),
-
-    // This makes our vendor bundle from node_modules modules.
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks(module) {
-        return module.context && module.context.indexOf('node_modules') !== -1
-      },
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
     }),
 
-    // This ensures that our vendor bundle name doesn't change between builds (unless the vendor contents change)
-    // by extracting out the webpack bootstrap code into its own file.
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      minChunks: Infinity,
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './_redirects',
+        },
+      ],
     }),
-
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        screw_ie8: true, // eslint-disable-line camelcase
-        warnings: false,
-      },
-      mangle: {
-        screw_ie8: true, // eslint-disable-line camelcase
-      },
-      output: {
-        comments: false,
-        screw_ie8: true, // eslint-disable-line camelcase
-      },
-    }),
-
-    new ExtractTextPlugin('css/[name].[contenthash].css'),
   ],
   resolve: {
-    modules: [
-      'node_modules',
-      buildConfig.paths.base,
-    ],
-    extensions: [
-      '.js',
-      '.jsx',
-    ],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    modules: ['node_modules', buildConfig.paths.src],
   },
 }
